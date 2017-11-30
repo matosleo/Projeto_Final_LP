75bbadb2f2d6a2e394ab6a60e04e660fd2249d27
gleydvan <gleyd_van@yahoo.com.br>
2017-11-30 10:36:47 -0300

    iniciando declaração do  pet fera

---
+++ include/Pet_Fera.hpp
@@ -0,0 +1,19 @@
+#ifndef PET_FERA_H
+#define PET_FERA_H
+
+class Pet_Fera
+{
+
+
+
+public:
+	Pet_Fera();
+	~Pet_Fera();
+
+	bool cadastrar_veterinario();
+	bool cadastrar_tratador();
+	bool cadastrar_animal();
+		
+};
+
+#endif
