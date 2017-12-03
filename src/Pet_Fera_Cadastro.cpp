05fa9932c3485f0365f944aad02b45c12359a289
João Vítor <jv.venceslau.c@gmail.com>
2017-12-03 10:31:49 -0300

    Arquivo para implementação dos métodos do Pet_Fera_Cadastro criado

---
+++ src/Pet_Fera_Cadastro.cpp
@@ -0,0 +1,132 @@
+/**
+ * @file Pet_Fera_Cadastro.cpp
+ * @brief      Implementação da classe Pet_Fera_Cadastro, seus atributos e métodos
+ * @details    
+ *
+ * @author     Gleydvan Macedo
+ * @author     João Vítor Venceslau Coelho
+ * @since      03/12/2017
+ * @date       03/12/2017
+ */
+
+#include "Pet_Fera_Cadastro.hpp"
+
+Pet_Fera_Cadastro::Pet_Fera_Cadastro()
+{
+
+}
+
+Pet_Fera_Cadastro::~Pet_Fera_Cadastro()
+{
+
+}
+
+bool Pet_Fera_Cadastro::cadastrar_veterinario()
+{
+
+}
+
+bool Pet_Fera_Cadastro::cadastrar_tratador()
+{
+
+}
+
+bool Pet_Fera_Cadastro::cadastrar_anfibio()
+{
+
+}
+
+bool Pet_Fera_Cadastro::cadastrar_mamifero()
+{
+
+}
+
+bool Pet_Fera_Cadastro::cadastrar_reptil()
+{
+
+}
+
+bool Pet_Fera_Cadastro::cadastrar_ave()
+{
+
+}
+
+bool Pet_Fera_Cadastro::remover_veterinario()
+{
+
+}
+
+bool Pet_Fera_Cadastro::remover_tratador()
+{
+
+}
+
+bool Pet_Fera_Cadastro::remover_anfibio()
+{
+
+}
+
+bool Pet_Fera_Cadastro::remover_mamifero()
+{
+
+}
+
+bool Pet_Fera_Cadastro::remover_reptil()
+{
+
+}
+
+bool Pet_Fera_Cadastro::remover_ave()
+{
+
+}
+
+bool Pet_Fera_Cadastro::alterar_anfibio()
+{
+
+}
+
+bool Pet_Fera_Cadastro::alterar_mamifero()
+{
+
+}
+
+bool Pet_Fera_Cadastro::alterar_reptil()
+{
+
+}
+
+bool Pet_Fera_Cadastro::alterar_ave()
+{
+
+}
+
+bool Pet_Fera_Cadastro::importar_dados_animais()
+{
+
+}
+
+bool Pet_Fera_Cadastro::importar_dados_funcionarios()
+{
+
+}
+
+bool Pet_Fera_Cadastro::exportar_dados_animais()
+{
+
+}
+
+bool Pet_Fera_Cadastro::exportar_dados_funcionarios()
+{
+
+}
+
+unsigned int Pet_Fera_Cadastro::quantidade_animais_cadastrados( void )
+{
+
+}
+
+unsigned int Pet_Fera_Cadastro::quantidade_funcionarios_cadastrados( void )
+{
+
+}
